4e32e8e883cd59d139e1b7562c60d98084b81df3
Micah Zaayer <micahzaayer@gmail.com>
2019-09-13 11:09:57 -0700

    seperated api calls for name and symptom with different inputs.

--- src/doctor.js
+++ src/doctor.js
@@ -1,20 +1,33 @@
 // Back-end
 
-export class DoctorFinder {
-  findDoc(lastName, symptom) {
+export class SymptomFinder {
+  findSymptom(symptom) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url =
+      const url =`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
 
-      `https://api.betterdoctor.com/2016-03-01/doctors?name=${lastName}&query=${symptom}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
 
 
-// //search by symptom
-// `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675%2C25&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
 
-// //search by last name
-// `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675%2C25&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
 
+export class NameFinder {
+  findName(lastName) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url =`https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -2,31 +2,53 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { DoctorFinder } from './doctor';
+import { NameFinder, SymptomFinder } from './doctor';
 
 $(document).ready(function () {
-  $("#findADoc").submit(function() {
+  $("#searchSymptom").submit(function() {
     event.preventDefault();
     const symptom = $("#symptomInput").val();
-    const lastName = $("#nameInput").val();
-    let doctorFinder = new DoctorFinder(lastName, symptom);
-    let promise = doctorFinder.findDoc(lastName, symptom);
+    let symptomFinder = new SymptomFinder(symptom);
+    let promise = symptomFinder.findSymptom(symptom);
 
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      console.log(body.data[0].profile.first_name);
+      // console.log(body.data[0].profile.first_name);
       console.log(body);
-      $(".doctorList").text(`Here is a list of Doctors that can help with your ${symptom} : ${body.data[0].profile.first_name}`);
+      // $(".doctorList").text(`Here is a list of Doctors: ${body.data[0]}`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      // ${body.data[2].profile.first_name}
+      // console.log(body.data.profile);
     });
-
-
-// ${body.data[2].profile.first_name}
-// console.log(body.data.profile);
-
-
-
   });
+
+
+
+  $("#searchName").submit(function() {
+    event.preventDefault();
+    const lastName = $("#nameInput").val();
+    let nameFinder = new NameFinder(lastName);
+    let promise2 = nameFinder.findName(lastName);
+
+
+    promise2.then(function(response) {
+      const body2 = JSON.parse(response);
+      // console.log(body.data[0].profile.first_name);
+      console.log(body2);
+      console.log(body2.data[0].practices[1]);
+      console.log(body2.data[0].practices[1,2,3,4,5,6,7,8,9,10]);
+      $(".doctorList").text(`Here is a list of Doctors: ${body2.data.practices}`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      // ${body.data[2].profile.first_name}
+      // console.log(body.data.profile);
+    });
+  });
+
+
+
+
+
 });

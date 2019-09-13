e367dd3c3208efcf907062202b5e70a7a7d62456
Micah Zaayer <micahzaayer@gmail.com>
2019-09-13 10:10:19 -0700

    add last-name search perameter.

--- src/doctor.js
+++ src/doctor.js
@@ -1,10 +1,19 @@
 // Back-end
 
 export class DoctorFinder {
-  findDoc(symptom) {
+  findDoc(lastName, symptom) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      const url =
+
+      `https://api.betterdoctor.com/2016-03-01/doctors?name=${lastName}&query=${symptom}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+
+
+// //search by symptom
+// `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675%2C25&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+
+// //search by last name
+// `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastName}&location=45.505%2C-122.675%2C25&user_location=45.505%2C-122.675%2C25&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
 
       request.onload = function() {
         if (this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -8,20 +8,23 @@ $(document).ready(function () {
   $("#findADoc").submit(function() {
     event.preventDefault();
     const symptom = $("#symptomInput").val();
-    let doctorFinder = new DoctorFinder(symptom);
-    let promise = doctorFinder.findDoc(symptom);
+    const lastName = $("#nameInput").val();
+    let doctorFinder = new DoctorFinder(lastName, symptom);
+    let promise = doctorFinder.findDoc(lastName, symptom);
 
 
     promise.then(function(response) {
       const body = JSON.parse(response);
-      console.log(body.data.profile);
-      $(".doctorList").text(`Here is a list of Doctors that can help with your ${symptom} : ${body.data[2].profile.first_name}`);
+      console.log(body.data[0].profile.first_name);
+      console.log(body);
+      $(".doctorList").text(`Here is a list of Doctors that can help with your ${symptom} : ${body.data[0].profile.first_name}`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
 
 
-
+// ${body.data[2].profile.first_name}
+// console.log(body.data.profile);
 
 
 
